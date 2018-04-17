78f344e3c77ac55d8513e4e159c89570f4e624d9
aalonzo <andre.alonzo@alliancestsl.com>
2018-04-17 11:08:00 -0400

    update header authorization

--- testfile.php
+++ testfile.php
@@ -4,23 +4,23 @@ header("Access-Control-Allow-Headers: Origin, X-Request-With");
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 header('Content-Type: application/json');
 
-$token = null;
-$headers = apache_request_headers();
-if(isset($headers['authorization'])){
+$username = null;
+$password = null;
+
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+    $username = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
     
-    $matches = array();
-        preg_match('/Token token="(.*)"/', $headers['authorization'], $matches);
-        if(isset($matches[1])){
-            $token = $matches[1];
-        }
+    $ret = [
+        'user' => $username,
+        'pass' => $password,
+    ];
+    
+    print json_encode($ret);
 } 
-/*$ret = [
-        'result' => $_SERVER['REQUEST_METHOD'],
-    ];*/
-    print json_encode($headers);
 
 //references
 //required headers -  https://stackoverflow.com/questions/8719276/cors-with-php-headers/9866124
-//athorization header - https://stackoverflow.com/questions/2902621/fetching-custom-authorization-header-from-incoming-php-request
+//athorization header - https://evertpot.com/223/
 ?>
 
